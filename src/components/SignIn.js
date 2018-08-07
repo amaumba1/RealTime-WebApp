04d22cdd5c9607c15601a6d7b9c52bee17ed9327
Ally Maumba <amaumba1@gmail.com>
2018-08-06 23:21:19 -0500

    add a signup logic

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -1,4 +1,8 @@
-import React from 'react'
+import { withRouter } from 'react-router-dom';
+import React, { Component } from 'react';
+import { SignUpLink } from './SignUp'; 
+import { auth } from '../firebase';
+import * as routes from '../constants/routes'; 
 
 const SignInPage = () =>
     <div>
