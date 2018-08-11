80b14cb83337c353c0210d00274ee709ce0b5945
Ally Maumba <amaumba1@gmail.com>
2018-08-11 00:06:32 -0500

    account

--- src/components/Account.js
+++ src/components/Account.js
@@ -1,7 +1,15 @@
-import React from 'react'
-
-const AccountPage = () => <div>
-    <div>Account Page</div>
-</div>
-
+import React from 'react';
+import AuthUserContext from './AuthUserContext';
+import { PasswordForgetForm } from './PasswordForget';
+import PasswordChangeForm from './PasswordChange';
+const AccountPage = () =>
+    <AuthUserContext.Consumer>
+        {authUser =>
+            <div>
+                <h1>Account: {authUser.email}</h1>
+                <PasswordForgetForm />
+                <PasswordChangeForm />
+            </div>
+        }
+    </AuthUserContext.Consumer>
 export default AccountPage;
